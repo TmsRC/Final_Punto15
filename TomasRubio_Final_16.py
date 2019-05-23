16aef4f3f09ac0804f124db47d4cfccf37465d92
TmsRC <t.rubio28@uniandes.edu.co>
2019-05-23 16:49:28 +0000

    Ahí va algo, pero aun no da respuestas convincentes :/

--- TomasRubio_Final_16.py
+++ TomasRubio_Final_16.py
@@ -7,31 +7,33 @@ dt = 1;
 
 def logprior(v,x,y,t):
     if v > 5000 or v < 360:
-        return 0
+        return -np.inf
     elif t > 3800 or t < 0:
-        return 0
+        return -np.inf
     else:
-        return 1
+        return 0
 
 def loglikelihood(v,x,y,t):
     distx = x_barcos-x
     disty = y_barcos-y
+    demora = ts-t
     dist = np.sqrt(distx**2+disty**2)
-    epsilon = dist - v*(ts-t)
+    epsilon = dist - v*(demora)
     chi2 = np.square(np.divide(epsilon,v*1))
+    
     return -(1/2)*np.sum(chi2)
 
 def mcmc(n_points):
     N = n_points
     v = [1000]
-    x = [1.0*np.random.randint(-100,100)]
-    y = [1.0*np.random.randint(-100,100)]
+    x = [1.0*np.random.randint(-300,300)]
+    y = [1.0*np.random.randint(-300,300)]
     t = [0.0]
     for i in range(N):
-        x_new = x[i] + np.random.normal(loc=0,scale=10)
-        y_new = y[i] + np.random.normal(loc=0,scale=10)
-        v_new = v[i] + np.random.normal(loc=0,scale=10)
-        t_new = t[i] + np.random.normal(loc=0,scale=50)
+        x_new = x[i] + np.random.normal(loc=0,scale=5)
+        y_new = y[i] + np.random.normal(loc=0,scale=5)
+        v_new = v[i] + np.random.normal(loc=0,scale=5)
+        t_new = t[i] + np.random.normal(loc=0,scale=10)
         
         logViejo = loglikelihood(v[i],x[i],y[i],t[i]) + logprior(v[i],x[i],y[i],t[i])
         logNuevo = loglikelihood(v_new,x_new,y_new,t_new) + logprior(v_new,x_new,y_new,t_new)
@@ -47,10 +49,12 @@ def mcmc(n_points):
             v.append(v_new)
             x.append(x_new)
             y.append(y_new)
+            t.append(t_new)
         else:
             v.append(v[i])
             x.append(x[i])
             y.append(y[i])
+            t.append(t[i])
     
     vfin = np.mean(np.array(v))
     sigmav = np.std(np.array(v))
@@ -58,11 +62,13 @@ def mcmc(n_points):
     sigmax = np.std(np.array(x))
     yfin = np.mean(np.array(y))
     sigmay = np.std(np.array(y))
-    return vfin,sigmav,xfin,sigmax,yfin,sigmay
+    tfin = np.mean(np.array(t))
+    sigmat = np.std(np.array(t))
+    return vfin,sigmav,xfin,sigmax,yfin,sigmay,tfin,sigmat
 
-v,sv,x,sx,y,sy = mcmc(1000)
+v,sv,x,sx,y,sy,t,st = mcmc(1000000)
 
 print("coordenada x:",x,"+/-",sx)
 print("coordenada y:",y,"+/-",sy)
-print("tiempo lanzamiento:","+/-",)
+print("tiempo lanzamiento:",t,"+/-",st)
 print("velocidad del sonido:",v,"+/-",sv)
